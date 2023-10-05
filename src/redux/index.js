9ea6c37c3995e5934656f5529705efc5b2c466f0
=Andrei <balasca.andrei@gmail.com>
2023-10-05 21:40:28 +0300

    Update index.js

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,10 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchWrapper } from '../helpers/fetch-wrapper';
-import { history } from '../helpers/history';
+import { configureStore } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { fetchWrapper } from "../helpers/fetch-wrapper";
+import { history } from "../helpers/history";
 
 // create slice
-const name = 'auth';
+const name = "auth";
 const initialState = createInitialState();
 const reducers = createReducers();
 const extraActions = createExtraActions();
@@ -17,51 +17,55 @@ export const authReducer = slice.reducer;
 function createInitialState() {
   return {
     // initialize state from local storage to enable user to stay logged in
-    user: JSON.parse(localStorage.getItem('user')),
-    error: null
-  }
+    user: JSON.parse(localStorage.getItem("user")),
+    error: null,
+  };
 }
 function createReducers() {
   return {
-    logout
+    logout,
   };
   function logout(state) {
     state.user = null;
-    localStorage.removeItem('user');
-    history.navigate('/login');
+    localStorage.removeItem("user");
+    history.navigate("/login");
   }
 }
 
 function createExtraActions() {
-  const baseUrl = `http://localhost:5000`; // http://188.24.131.206:6969
+  const baseUrl = `http://188.24.131.206:6969`; // http://188.24.131.206:6969 http://localhost:5000
   return {
     login: login(),
-    register: register()
+    register: register(),
   };
   function login() {
     return createAsyncThunk(
       `${name}/login`,
-      async ({ email, pass }) => await fetchWrapper.post(`${baseUrl}/login`, { email, pass })
+      async ({ email, pass }) =>
+        await fetchWrapper.post(`${baseUrl}/login`, { email, pass })
     );
-  };
-
+  }
 
   function register() {
     return createAsyncThunk(
       `${name}/register`,
-      async ({ email, pass }) => await fetchWrapper.post(`${baseUrl}/register`, { email, pass })
+      async ({ email, pass }) =>
+        await fetchWrapper.post(`${baseUrl}/register`, { email, pass })
     );
   }
-
 }
 
 function createExtraReducers() {
   return {
-    ...login()
+    ...login(),
   };
   function login() {
     var { pending, fulfilled, rejected } = extraActions.login;
-    var { pending: pendingRegister, fulfilled: fulfilledRegister, rejected: rejectedRegister } = extraActions.register;
+    var {
+      pending: pendingRegister,
+      fulfilled: fulfilledRegister,
+      rejected: rejectedRegister,
+    } = extraActions.register;
     const loginActions = {
       [pending]: (state) => {
         state.error = null;
@@ -72,35 +76,35 @@ function createExtraReducers() {
       [fulfilled]: (state, action) => {
         const user = action.payload;
         // store user details and jwt token in local storage to keep user logged in between page refreshes
-        localStorage.setItem('user', JSON.stringify(user));
+        localStorage.setItem("user", JSON.stringify(user));
         state.user = user;
         // get return url from location state or default to home page
-        const { from } = history.location.state || { from: { pathname: '/' } };
+        const { from } = history.location.state || { from: { pathname: "/" } };
         history.navigate(from);
-      }
-    }
+      },
+    };
 
     const registerActions = {
       [fulfilledRegister]: (state, action) => {
         const user = action.payload;
         // store user details and jwt token in local storage to keep user logged in between page refreshes
-        localStorage.setItem('user', JSON.stringify(user));
+        localStorage.setItem("user", JSON.stringify(user));
         state.user = user;
         // get return url from location state or default to home page
-        const { from } = history.location.state || { from: { pathname: '/' } };
-        history.navigate('/login');
+        const { from } = history.location.state || { from: { pathname: "/" } };
+        history.navigate("/login");
       },
       [rejected]: (state, action) => {
         state.error = action.error;
       },
       [rejectedRegister]: (state, action) => {
         state.error = action.error;
-      }
+      },
     };
     return {
       ...loginActions,
-      ...registerActions
-    }
+      ...registerActions,
+    };
   }
 }
 
